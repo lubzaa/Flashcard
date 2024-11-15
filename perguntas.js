9ea06a361603e3258d370eaa9d42487ea932686c
Analu <00001120732360SP@al.educacao.sp.gov.br>
2024-11-15 12:29:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'História',
+    'Qual o nome do primeiro presidente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o nome do hormônio que regula o sono?',
+    'Melatonina'
+)
+
+criaCartao(
+    'Geografia',
+    'Como se chama a ciência que estuda mapas?',
+    'Cartografia'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'Qual é a classe gramatical que indica ação?',
+    'Verbo'
+)
